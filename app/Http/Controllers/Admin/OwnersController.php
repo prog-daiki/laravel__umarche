c407274b7fc0b019209d42f247658846c8b0e04f
prog-daiki <t.daiki0913@icloud.com>
2023-02-09 15:19:21 +0900

    shopCreateDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -5,10 +5,14 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Owner; //エロクアント
+use App\Models\Shop; //エロクアント
 use Illuminate\Support\Facades\DB; //クエリビルダ
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
+use Throwable;
+use Illuminate\Support\Facades\Log;
+
 
 
 class OwnersController extends Controller
@@ -63,11 +67,27 @@ class OwnersController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        Owner::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        try{
+            DB::transaction(function() use ($request){
+                $owner=Owner::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                ]);
+                Shop::create([
+                    'owner_id' => $owner->id,
+                    'name' => '店名を入力して下さい',
+                    'information' =>'',
+                    'filename' => '',
+                    'is_selling' => true
+                ]);
+            },2);
+        }
+        catch(Throwable $e){
+            Log::error($e);
+            throw $e;
+        }
+
         return redirect()->route('admin.owners.index')
         ->with(['message' => 'オーナー登録を実施しました。',
                 'status' => 'info']);
