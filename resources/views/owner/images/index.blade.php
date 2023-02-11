6d30293b7ad018e7c3fba54017ce15e113749342
prog-daiki <t.daiki0913@icloud.com>
2023-02-11 14:41:35 +0900

    store

--- resources/views/owner/images/index.blade.php
+++ resources/views/owner/images/index.blade.php
@@ -13,16 +13,18 @@
                         <div class="flex justify-end mb-4 mr-48">
                             <button onclick="location.href='{{route('owner.images.create')}}' " class=" text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">新規登録する</button>
                         </div>
+                    <div class="flex flex-wrap">
                     @foreach($images as $image)
                     <div class="w-1/4 p-4">
                         <a href="{{route('owner.images.edit',['image'=>$image->id])}}">
                         <div class="border rounded-md p-4">
                             <div class="text-x1">{{$image->title}}</div>
-                            <x-thumbnail :filename="$shop->filename" type="products"></x-thumbnail>
+                            <x-thumbnail :filename="$image->filename" type="products"></x-thumbnail>
                         </div>
                         </div>
                         </a>
                     @endforeach
+                    </div>
                     {{$images->Links()}}
                 </div>
             </div>
