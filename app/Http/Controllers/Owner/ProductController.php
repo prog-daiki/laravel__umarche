456f9dbd30c4c9ddb6d677aa6b1f92019a4dd7ad
prog-daiki <t.daiki0913@icloud.com>
2023-02-14 13:52:42 +0900

    product index, eager loading

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -3,18 +3,48 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Models\Image;
+use App\Models\SecondaryCategory;
+use App\Models\Owner;
+use Illuminate\Support\Facades\Auth;
+
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+            $id = $request->route()->parameter('product'); //shopのid取得
+            if (!is_null($id)) { // null判定
+                $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+                $productId = (int)$productsOwnerId; // キャスト 文字列→数値に型変換
+                if ($productId !== Auth::id()) { // 同じでなかったら
+                    abort(404); // 404画面表示 }
+                }
+            }
+            return $next($request);
+        });
+    }
+
     public function index()
     {
-        //
+        // $products = Owner::findOrFail(Auth::id())->shop->product;
+
+        $ownerInfo = Owner::with('shop.product.imageFirst')->where('id', Auth::id())->get();
+
+        // // dd($ownerInfo);
+        // foreach ($ownerInfo as $owner) {
+        //     // dd($owner->shop->product);
+        //     foreach ($owner->shop->product as $product) {
+        //         dd($product->imageFirst->filename);
+        //     };
+        // }
+
+        return view('owner.products.index', compact('ownerInfo'));
     }
 
     /**
