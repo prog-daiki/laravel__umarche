787e14e77fbdaa5b9677945b308108306bf05e8d
prog-daiki <t.daiki0913@icloud.com>
2023-02-18 15:01:56 +0900

    cart relation

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -99,7 +99,7 @@
   </div>
 
   <div class="modal micromodal-slide" id="modal-1" aria-hidden="true">
-    <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+    <div class="modal__overlay z-10" tabindex="-1" data-micromodal-close>
       <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
         <header class="modal__header">
           <h2 class="text-xl text-gray-700" id="modal-1-title">
